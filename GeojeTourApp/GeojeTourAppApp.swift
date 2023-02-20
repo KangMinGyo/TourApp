f5cc5b779b420701dd4a6fd457e9d53157d2c881
KangMinGyo <kang15567@kakao.com>
2023-02-20 14:39:42 +0900

    First Commit

---
+++ GeojeTourApp/GeojeTourAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeojeTourAppApp.swift
+//  GeojeTourApp
+//
+//  Created by KangMingyo on 2023/02/20.
+//
+
+import SwiftUI
+
+@main
+struct GeojeTourAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
